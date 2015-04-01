71c789fa73d1ca5626fc3f57c49f32e890ebe09e
Benjamin Murphy <benjamin_murphy@me.com>
2015-04-01 18:58:52 -0400

    added dynamic growing/shrinking.
    Known bugs: does not support more than two nested blocks

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,32 +1,104 @@
+var copynum = 0;
+
+var playground = document.getElementById("playground");
+var menu = document.getElementById("menu");
+
+var droppableWidth = 100;
+
 function allowDrop(event) {
     event.preventDefault();
 }
 
-function drag(event) {
-    event.dataTransfer.setData("text", event.target.id);
-}
-
 function drop(event) {
-    event.preventDefault();
+
+    var target = event.target;
     var data = event.dataTransfer.getData("text");
     var element = document.getElementById(data);
-    element.style.display = "block";
-    event.target.appendChild(element);
+
+    if (target.classList.contains("droppable") && playground.contains(target)) {
+        event.preventDefault();
+        element.style.display = "block";
+        target.appendChild(element);
+        element.classList.add("shrinkable");
+        growParents(element);
+    } else if (playground.contains(target)) {
+        event.preventDefault();
+        element.style.display = "block";
+        playground.appendChild(element);
+    } else if (menu.contains(target)) {
+        event.preventDefault();
+        element.parentNode.removeChild(element);
+    }
 }
 
-function trash(event) {
-    event.preventDefault();
-    var data = event.dataTransfer.getData("text");
-    var el = document.getElementById(data);
-    el.parentNode.removeChild(el);
-}
-
-function dragCopy(event) {
+function startDrag(event) {
     var original = document.getElementById(event.target.id);
-    var copy = original.cloneNode(false);
-    copy.id = "copy";
-    copy.style.display = "none";
-    original.appendChild(copy);
+    var copy = null;
     
-    event.dataTransfer.setData("text", "copy");
+    if (original.classList.contains("shrinkable")) {
+            original.classList.remove("shrinkable");
+            shrinkParents(original);
+    }
+
+    if (playground.contains(original)) {
+        copy = original;
+    } else if (menu.contains(original)) {
+        copy = original.cloneNode(true);
+        copy.id = "copy_" + copynum.toString();
+        copynum += 1;
+        copy.style.display = "none";
+        original.appendChild(copy);
+    }
+
+    event.dataTransfer.setData("text", copy.id);
+}
+
+function isInDroppable(element) {
+    var currentElement = element.parentNode;
+    while (currentElement != document.body && currentElement.id !== "playground" && currentElement.id !== "menu") {
+        if (currentElement.classList.contains("droppable")) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function growParents(element) {
+    var currentElement = element.parentNode;
+    while (currentElement.id !== "playground" && currentElement.id !== "menu") {
+        currentElement.grow(element.scrollWidth);
+        currentElement = currentElement.parentNode;
+    }
+}
+
+function shrinkParents(element) {
+    var currentElement = element.parentNode;
+    while (currentElement.id !== "playground" && currentElement.id !== "menu") {
+        currentElement.shrink(element.scrollWidth);
+        currentElement = currentElement.parentNode;
+    }
+}
+
+HTMLDivElement.prototype.grow = function(width) {    
+    var newHeight = this.offsetHeight + 20;
+    this.style.height = newHeight + 'px';
+    var newWidth = width;
+
+    if (!this.classList.contains("droppable")) {
+        newWidth += this.scrollWidth;
+    }
+
+    this.style.width = newWidth + 'px';
+}
+
+HTMLDivElement.prototype.shrink = function(width) {
+    var newHeight = this.scrollHeight - 20;
+    this.style.height = newHeight + 'px';
+    var newWidth = this.offsetWidth  - width;
+    
+    if (this.classList.contains("droppable")) {
+        newWidth += droppableWidth;
+    }
+    
+    this.style.width = newWidth + 'px';
 }
