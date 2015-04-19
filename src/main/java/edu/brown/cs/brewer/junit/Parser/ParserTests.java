2ba7237b6e240cac65b61730f4a60605311d6096
Di Yang Shi <steven200796@gmail.com>
2015-04-19 14:31:20 -0400

    Started Testing for Parsing
    Added two basic tests to parsing, will add more after getting JSON
    strings for tests.

--- src/main/java/edu/brown/cs/brewer/junit/Parser/ParserTests.java
+++ src/main/java/edu/brown/cs/brewer/junit/Parser/ParserTests.java
@@ -1,7 +1,9 @@
 package edu.brown.cs.brewer.junit.Parser;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.util.Arrays;
 import java.util.Map;
 
 import org.junit.Before;
@@ -13,7 +15,7 @@ import edu.brown.cs.brewer.BrewerRuntime;
 import edu.brown.cs.brewer.Variable;
 import edu.brown.cs.brewer.handlers.Parser;
 import edu.brown.cs.brewer.handlers.Parser.BrewerParseException;
-import edu.brown.cs.brewer.handlers.Stream;
+import edu.brown.cs.brewer.handlers.Stream.Runner;
 
 /**
  * The Junit test for the Parser.java class.
@@ -23,28 +25,52 @@ import edu.brown.cs.brewer.handlers.Stream;
  *
  */
 public class ParserTests {
-
-  String t1 =
-      "{\"main\":[{\"type\":\"set\",\"name\":\"x\",\"class\":\"number\"},\"value\":{\"type\":\"literal\",\"value\":\"5\",\"class\":\"number\"}},{\"type\":\"get\",\"name\":\"x\"}]}";
-
   Gson GSON = new Gson();
-
-  Stream.Runner runner;
+  BrewerRuntime br;
 
   @Before
-  public void init() {}
+  public void init() {
+  }
 
   @Test
-  public void test() {
-    BrewerRuntime runtime;
+  public void test1() {
+    String setGet = "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"name\":\"x\",\"class\":\"number\"},\"value\":{\"type\":\"literal\",\"value\":5,\"class\":\"number\"}},{\"type\":\"get\",\"name\":{\"type\":\"var\",\"name\":\"x\",\"class\":\"number\"}}]}";
     try {
-      runtime = Parser.parseJSONProgram(t1, null);
-      Map<String, Variable> vars = runtime.getVariables();
-      Object value = vars.get("\"x\"").getValue();
-      assertTrue((int) value == 5);
+      br = Parser.parseJSONProgram(setGet, null);
+      br.setRunner(new Runner(null, 0));
+      br.run();
+
+      Map<String, Variable> vars = br.getVariables();
+      Object value = vars.get("x").getValue();
+      assertTrue((double) value == 5);
+
     } catch (BrewerParseException e) {
-      // TODO auto-generated
+      fail("test1 failed");
       e.printStackTrace();
     }
+  }
+
+  public void test2() {
+    String factorial = "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"},\"value\":{\"type\":\"literal\",\"value\":6,\"class\":\"number\"}},{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"},\"value\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"}},{\"type\":\"while\",\"condition\":{\"type\":\"comparison\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"arg2\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"},\"name\":\"greater\"},\"commands\":[{type : \"print\", name : \"b\"}, {\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"},\"value\":{\"type\":\"numeric_operator\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"}},\"arg2\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"name\":\"mul\"}},{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"},\"value\":{\"type\":\"numeric_operator\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"arg2\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"},\"name\":\"sub\"}}]}, {type : \"print\", name : \"b\"}]}";
+    try {
+      br = Parser.parseJSONProgram(factorial, null);
+      br.setRunner(new Runner(null, 0));
+      br.run();
+
+      String outputLog = Arrays.toString(br.getLogs().toArray());
+      String expectedLog = "[Log: MESSAGE: b: 1.0, Log: MESSAGE: b: 6.0, Log: MESSAGE: b: 30.0, Log: MESSAGE: b: 120.0, Log: MESSAGE: b: 360.0, Log: MESSAGE: b: 720.0]";
+      assertTrue(expectedLog.equals(outputLog));
+
+      Map<String, Variable> vars = br.getVariables();
+      Object value = vars.get("b").getValue();
+      assertTrue((double) value == 720.0);
+
+    } catch (BrewerParseException e) {
+      fail("test2 failed");
+      e.printStackTrace();
+    }
+  }
+  
+  public void test3() {
   }
 }
