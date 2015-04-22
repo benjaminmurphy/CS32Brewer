4f70dd76cf389ccbf8281ef0987b55a470f96462
Raphael Kargon <raphael.kargon@gmail.com>
2015-04-22 17:32:17 -0400

    Fixed errors in tests, Main

--- src/main/java/edu/brown/cs/brewer/Main.java
+++ src/main/java/edu/brown/cs/brewer/Main.java
@@ -1,32 +1,26 @@
 package edu.brown.cs.brewer;
 
-import edu.brown.cs.brewer.handlers.BrewerServer;
 
 public class Main {
   private static final int DEFAULT_PORT = 4567;
 
   public static void main(String[] args) {
-    
-    BrewerServer.runServer(DEFAULT_PORT);
-    
-    
+
+    // BrewerServer.runServer(DEFAULT_PORT);
+
+
     // Server server = new Server(DEFAULT_PORT);
 
     // testing json parser
     /*
-    String factorial =
-        "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"},\"value\":{\"type\":\"literal\",\"value\":6,\"class\":\"number\"}},{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"},\"value\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"}},{\"type\":\"while\",\"condition\":{\"type\":\"comparison\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"arg2\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"},\"name\":\"greater\"},\"commands\":[{type : \"print\", name : \"b\"}, {\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"},\"value\":{\"type\":\"numeric_operator\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"}},\"arg2\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"name\":\"mul\"}},{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"},\"value\":{\"type\":\"numeric_operator\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"arg2\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"},\"name\":\"sub\"}}]}, {type : \"print\", name : \"b\"}]}";
-    BrewerRuntime br;
-    try {
-      br = Parser.parseJSONProgram(factorial, null);
-      br.setRunner(new Runner(null, 0));
-      br.run();
-      System.out.println(Arrays.toString(br.getLogs().toArray()));
-    } catch (BrewerParseException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    */
+     * String factorial =
+     * "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"},\"value\":{\"type\":\"literal\",\"value\":6,\"class\":\"number\"}},{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"},\"value\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"}},{\"type\":\"while\",\"condition\":{\"type\":\"comparison\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"arg2\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"},\"name\":\"greater\"},\"commands\":[{type : \"print\", name : \"b\"}, {\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"},\"value\":{\"type\":\"numeric_operator\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"}},\"arg2\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"name\":\"mul\"}},{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"},\"value\":{\"type\":\"numeric_operator\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"arg2\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"},\"name\":\"sub\"}}]}, {type : \"print\", name : \"b\"}]}"
+     * ; BrewerRuntime br; try { br = Parser.parseJSONProgram(factorial, null);
+     * br.setRunner(new Runner(null, 0)); br.run();
+     * System.out.println(Arrays.toString(br.getLogs().toArray())); } catch
+     * (BrewerParseException e) { // TODO Auto-generated catch block
+     * e.printStackTrace(); }
+     */
 
     // A simple example program:
     // BrewerRuntime simplebr = new BrewerRuntime();
--- src/main/java/edu/brown/cs/brewer/junit/Parser/ParserTests.java
+++ src/main/java/edu/brown/cs/brewer/junit/Parser/ParserTests.java
@@ -6,8 +6,10 @@ import static org.junit.Assert.fail;
 import java.util.Arrays;
 import java.util.Map;
 
+import org.json.simple.parser.ParseException;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.Runner;
 
 import com.google.gson.Gson;
 
@@ -28,48 +30,47 @@ public class ParserTests {
   BrewerRuntime br;
 
   @Before
-  public void init() {
-  }
+  public void init() {}
 
   @Test
   public void test1() {
-    String setGet = "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"name\":\"x\",\"class\":\"number\"},\"value\":{\"type\":\"literal\",\"value\":5,\"class\":\"number\"}},{\"type\":\"get\",\"name\":{\"type\":\"var\",\"name\":\"x\",\"class\":\"number\"}}]}";
+    String setGet =
+        "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"name\":\"x\",\"class\":\"number\"},\"value\":{\"type\":\"literal\",\"value\":5,\"class\":\"number\"}},{\"type\":\"get\",\"name\":{\"type\":\"var\",\"name\":\"x\",\"class\":\"number\"}}]}";
     try {
       br = Parser.parseJSONProgram(setGet);
-      br.setRunner(new Runner(null, 0));
       br.run();
 
       Map<String, Variable> vars = br.getVariables();
       Object value = vars.get("x").getValue();
       assertTrue((double) value == 5);
 
-    } catch (BrewerParseException e) {
+    } catch (BrewerParseException | ParseException e) {
       fail("test1 failed");
       e.printStackTrace();
     }
   }
 
   public void test2() {
-    String factorial = "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"},\"value\":{\"type\":\"literal\",\"value\":6,\"class\":\"number\"}},{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"},\"value\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"}},{\"type\":\"while\",\"condition\":{\"type\":\"comparison\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"arg2\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"},\"name\":\"greater\"},\"commands\":[{type : \"print\", name : \"b\"}, {\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"},\"value\":{\"type\":\"numeric_operator\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"}},\"arg2\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"name\":\"mul\"}},{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"},\"value\":{\"type\":\"numeric_operator\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"arg2\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"},\"name\":\"sub\"}}]}, {type : \"print\", name : \"b\"}]}";
+    String factorial =
+        "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"},\"value\":{\"type\":\"literal\",\"value\":6,\"class\":\"number\"}},{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"},\"value\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"}},{\"type\":\"while\",\"condition\":{\"type\":\"comparison\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"arg2\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"},\"name\":\"greater\"},\"commands\":[{type : \"print\", name : \"b\"}, {\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"},\"value\":{\"type\":\"numeric_operator\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"b\"}},\"arg2\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"name\":\"mul\"}},{\"type\":\"set\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"},\"value\":{\"type\":\"numeric_operator\",\"arg1\":{\"type\":\"get\",\"name\":{\"type\":\"var\",\"class\":\"number\",\"name\":\"a\"}},\"arg2\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"},\"name\":\"sub\"}}]}, {type : \"print\", name : \"b\"}]}";
     try {
-      br = Parser.parseJSONProgram(factorial, null);
-      br.setRunner(new Runner(null, 0));
+      br = Parser.parseJSONProgram(factorial);
       br.run();
 
       String outputLog = Arrays.toString(br.getLogs().toArray());
-      String expectedLog = "[Log: MESSAGE: b: 1.0, Log: MESSAGE: b: 6.0, Log: MESSAGE: b: 30.0, Log: MESSAGE: b: 120.0, Log: MESSAGE: b: 360.0, Log: MESSAGE: b: 720.0]";
+      String expectedLog =
+          "[Log: MESSAGE: b: 1.0, Log: MESSAGE: b: 6.0, Log: MESSAGE: b: 30.0, Log: MESSAGE: b: 120.0, Log: MESSAGE: b: 360.0, Log: MESSAGE: b: 720.0]";
       assertTrue(expectedLog.equals(outputLog));
 
       Map<String, Variable> vars = br.getVariables();
       Object value = vars.get("b").getValue();
       assertTrue((double) value == 720.0);
 
-    } catch (BrewerParseException e) {
+    } catch (BrewerParseException | ParseException e) {
       fail("test2 failed");
       e.printStackTrace();
     }
   }
-  
-  public void test3() {
-  }
+
+  public void test3() {}
 }
