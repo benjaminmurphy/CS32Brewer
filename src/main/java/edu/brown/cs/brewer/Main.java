a1451cb3297b74c9f0980f4a263f64bd2337bf59
Raphael Kargon <raphael.kargon@gmail.com>
2015-04-25 14:14:15 -0400

    Main now runs brewerserver

--- src/main/java/edu/brown/cs/brewer/Main.java
+++ src/main/java/edu/brown/cs/brewer/Main.java
@@ -1,41 +1,13 @@
 package edu.brown.cs.brewer;
 
-import java.util.Map;
-
-import spark.ModelAndView;
-import spark.Request;
-import spark.Response;
-import spark.Spark;
-import spark.TemplateViewRoute;
-import spark.template.freemarker.FreeMarkerEngine;
-
-import com.google.common.collect.ImmutableMap;
+import edu.brown.cs.brewer.handlers.BrewerServer;
 
 
 public class Main {
   private static final int DEFAULT_PORT = 4567;
 
   public static void main(String[] args) {
-    runSpankServer();
+    BrewerServer.runServer(DEFAULT_PORT);
   }
 
-  public static void runSpankServer() {
-    Spark.setPort(DEFAULT_PORT);
-    Spark.externalStaticFileLocation("src/main/resources/static");
-    Spark.get("/brewer", new GetHandler(), new FreeMarkerEngine());
-  }
-
-  /**
-   * This class serves the GUI to the client via a webserver.
-   *
-   * @author raphaelkargon
-   *
-   */
-  private static class GetHandler implements TemplateViewRoute {
-    @Override
-    public ModelAndView handle(final Request req, final Response res) {
-      Map<String, Object> variables = ImmutableMap.of();
-      return new ModelAndView(variables, "main.ftl");
-    }
-  }
 }
