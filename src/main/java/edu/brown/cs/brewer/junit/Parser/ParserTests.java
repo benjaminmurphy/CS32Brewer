77982a8f7025fee3c0a54c1cd0d71b54f686f439
Di Yang Shi <steven200796@gmail.com>
2015-04-28 21:39:33 -0400

    Added some more tests
    Still missing basic tests for And, Or, Not, While, Arithmetic.

--- src/main/java/edu/brown/cs/brewer/junit/Parser/ParserTests.java
+++ src/main/java/edu/brown/cs/brewer/junit/Parser/ParserTests.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.json.simple.parser.ParseException;
 import org.junit.Before;
 import org.junit.Test;
+
 import com.google.gson.Gson;
 
 import edu.brown.cs.brewer.BrewerRuntime;
@@ -35,7 +36,13 @@ public class ParserTests {
   public void init() {
   }
 
-  // Simple setGet test
+  // Basic functionality testing of blocks
+
+  // Simple setGet test with number type
+  /**
+   * x = 5;
+   * get(x) == 5;
+   */
   @Test
   public void setGet() {
     String setGet = "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"name\":\"x\",\"class\":\"number\"},\"value\":{\"type\":\"literal\",\"value\":5,\"class\":\"number\"}},{\"type\":\"get\",\"name\":{\"type\":\"var\",\"name\":\"x\",\"class\":\"number\"}}]}";
@@ -52,6 +59,278 @@ public class ParserTests {
       e.printStackTrace();
     }
   }
+
+  // Simple setPrint test with String type
+  /**
+   * x = "foo";
+   * print(x);
+   */
+  @Test
+  public void setPrint() {
+    String setPrint = "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"name\":\"x\",\"class\":\"string\"},\"value\":{\"type\":\"literal\",\"value\":\"foo\",\"class\":\"string\"}},{\"type\":\"print\",\"name\":{\"type\":\"var\",\"name\":\"x\",\"class\":\"string\"}}]}";
+
+    String setValue = "foo";
+    try {
+      br = Parser.parseJSONProgram(setPrint);
+      br.run();
+
+      Map<String, Variable> vars = br.getVariables();
+      String value = (String) vars.get("x").getValue();
+
+      assertTrue(value.equals(setValue));
+
+      String outputLog = Arrays.toString(br.getLogs().toArray());
+      String expectedLog = String
+        .format("[Log: MESSAGE: x: %s]", setValue);
+
+      assertTrue(outputLog.equals(expectedLog));
+    } catch (BrewerParseException | ParseException e) {
+      fail("setPrint test failed");
+      e.printStackTrace();
+    }
+  }
+
+  // Simple ifThen test, if clause pass
+  /**
+   * if true:
+   *    print(success!);
+   */
+  @Test
+  public void ifThenIfClause() {
+    String ifThenIfClause = "{\"main\":[{\"type\":\"if\",\"commands\":[{\"type\":\"print\",\"name\":{\"type\":\"literal\",\"value\":\"success!\",\"class\":\"string\"}}],\"condition\":{\"type\":\"literal\",\"value\":true,\"class\":\"bool\"}}]}";
+    String expected = "success!";
+    try {
+      br = Parser.parseJSONProgram(ifThenIfClause);
+      br.run();
+
+      String outputLog = Arrays.toString(br.getLogs().toArray());
+      String expectedLog = String.format("[Log: MESSAGE: %s]", expected);
+
+      assertTrue(outputLog.equals(expectedLog));
+    } catch (BrewerParseException | ParseException e) {
+      fail("ifThenIfClause test failed");
+      e.printStackTrace();
+    }
+  }
+
+  // Simple ifThen test, if clause fail
+  /**
+   * if false:
+   *    print(failure!);
+   */
+  @Test
+  public void ifThenClauseFail() {
+    String ifThenClauseFail = "{\"main\":[{\"type\":\"if\",\"commands\":[{\"type\":\"print\",\"name\":{\"type\":\"literal\",\"value\":\"failure!\",\"class\":\"string\"}}],\"condition\":{\"type\":\"literal\",\"value\":false,\"class\":\"bool\"}}]}";
+    try {
+      br = Parser.parseJSONProgram(ifThenClauseFail);
+      br.run();
+
+      String outputLog = Arrays.toString(br.getLogs().toArray());
+      String emptyLog = "[]";
+
+      assertTrue(outputLog.equals(emptyLog));
+    } catch (BrewerParseException | ParseException e) {
+      fail("ifThenIfClause test failed");
+      e.printStackTrace();
+    }
+  }
+
+  // Simple ifElse test, if clause pass
+  /**
+   * if true:
+   *    print("success!");
+   * else:
+   *    print("sadness :(");
+   */
+  @Test
+  public void ifElseIfClause() {
+    String ifElseIfClause = "{\"main\":[{\"type\":\"ifelse\",\"condition\":{\"type\":\"literal\",\"value\":true,\"class\":\"bool\"},\"commands\":[{\"type\":\"print\",\"name\":{\"type\":\"literal\",\"value\":\"success!\",\"class\":\"string\"}}],\"else\":[{\"type\":\"print\",\"name\":{\"type\":\"literal\",\"value\":\"sadness :(\",\"class\":\"string\"}}]}]}";
+    String expected = "success!";
+    try {
+      br = Parser.parseJSONProgram(ifElseIfClause);
+      br.run();
+
+      String outputLog = Arrays.toString(br.getLogs().toArray());
+      String expectedLog = String.format("[Log: MESSAGE: %s]", expected);
+
+      assertTrue(outputLog.equals(expectedLog));
+    } catch (BrewerParseException | ParseException e) {
+      fail("ifElseIfClause test failed");
+      e.printStackTrace();
+    }
+  }
+
+  // Simple ifElse test, deferred to else clause
+  /**
+   * if false:
+   *    print("sadness :(");
+   * else:
+   *    print("success!");
+   */
+  @Test
+  public void ifElseElseClause() {
+    String ifElseElseClause = "{\"main\":[{\"type\":\"ifelse\",\"condition\":{\"type\":\"literal\",\"value\":false,\"class\":\"bool\"},\"commands\":[{\"type\":\"print\",\"name\":{\"type\":\"literal\",\"value\":\"sadness :(\",\"class\":\"string\"}}],\"else\":[{\"type\":\"print\",\"name\":{\"type\":\"literal\",\"value\":\"success!\",\"class\":\"string\"}}]}]}";
+    String expected = "success!";
+    try {
+      br = Parser.parseJSONProgram(ifElseElseClause);
+      br.run();
+
+      String outputLog = Arrays.toString(br.getLogs().toArray());
+      String expectedLog = String.format("[Log: MESSAGE: %s]", expected);
+
+      assertTrue(outputLog.equals(expectedLog));
+    } catch (BrewerParseException | ParseException e) {
+      fail("ifElseElseClause test failed");
+      e.printStackTrace();
+    }
+  }
+
+  // Simple Equality Testing
+  // Equal
+  /**
+   * a = (0 == 0);
+   * print a;
+   */
+  @Test
+  public void equalityEqual() {
+    String equalityEqual = "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"name\":\"a\",\"class\":\"bool\"},\"value\":{\"type\":\"comparison\",\"arg1\":{\"type\":\"literal\",\"value\":0,\"class\":\"number\"},\"arg2\":{\"type\":\"literal\",\"value\":0,\"class\":\"number\"},\"name\":\"eq\"}},{\"type\":\"print\",\"name\":{\"type\":\"var\",\"name\":\"a\",\"class\":\"bool\"}}]}";
+    boolean expected = true;
+    try {
+      br = Parser.parseJSONProgram(equalityEqual);
+      br.run();
+
+      String outputLog = Arrays.toString(br.getLogs().toArray());
+      String expectedLog = String
+        .format("[Log: MESSAGE: a: %s]", expected);
+
+      assertTrue(outputLog.equals(expectedLog));
+    } catch (BrewerParseException | ParseException e) {
+      fail("equalityEqual test failed");
+      e.printStackTrace();
+    }
+  }
+
+  // Not Equal
+  /**
+   * a = (0 == 1);
+   * print a;
+   */
+  @Test
+  public void equalityNotEqual() {
+    String equalityNotEqual = "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"name\":\"a\",\"class\":\"bool\"},\"value\":{\"type\":\"comparison\",\"arg1\":{\"type\":\"literal\",\"value\":0,\"class\":\"number\"},\"arg2\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"},\"name\":\"eq\"}},{\"type\":\"print\",\"name\":{\"type\":\"var\",\"name\":\"a\",\"class\":\"bool\"}}]}";
+    boolean expected = false;
+    try {
+      br = Parser.parseJSONProgram(equalityNotEqual);
+      br.run();
+
+      String outputLog = Arrays.toString(br.getLogs().toArray());
+      String expectedLog = String
+        .format("[Log: MESSAGE: a: %s]", expected);
+
+      assertTrue(outputLog.equals(expectedLog));
+    } catch (BrewerParseException | ParseException e) {
+      fail("equalityNotEqual test failed");
+      e.printStackTrace();
+    }
+  }
+
+  // Less Than
+  /**
+   * a = (0 < 1);
+   * print a;
+   */
+  @Test
+  public void equalityLessThan() {
+    String equalityLessThan = "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"name\":\"a\",\"class\":\"bool\"},\"value\":{\"type\":\"comparison\",\"arg1\":{\"type\":\"literal\",\"value\":0,\"class\":\"number\"},\"arg2\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"},\"name\":\"less\"}},{\"type\":\"print\",\"name\":{\"type\":\"var\",\"name\":\"a\",\"class\":\"bool\"}}]}";
+    boolean expected = true;
+    try {
+      br = Parser.parseJSONProgram(equalityLessThan);
+      br.run();
+
+      String outputLog = Arrays.toString(br.getLogs().toArray());
+      String expectedLog = String
+        .format("[Log: MESSAGE: a: %s]", expected);
+
+      assertTrue(outputLog.equals(expectedLog));
+    } catch (BrewerParseException | ParseException e) {
+      fail("equalityLessThan test failed");
+      e.printStackTrace();
+    }
+  }
+
+  // Not Less Than
+  /**
+   * a = (0 < 0);
+   * print a;
+   */
+  @Test
+  public void equalityNotLessThan() {
+    String equalityNotLessThan = "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"name\":\"a\",\"class\":\"bool\"},\"value\":{\"type\":\"comparison\",\"arg1\":{\"type\":\"literal\",\"value\":0,\"class\":\"number\"},\"arg2\":{\"type\":\"literal\",\"value\":0,\"class\":\"number\"},\"name\":\"less\"}},{\"type\":\"print\",\"name\":{\"type\":\"var\",\"name\":\"a\",\"class\":\"bool\"}}]}";
+    boolean expected = false;
+    try {
+      br = Parser.parseJSONProgram(equalityNotLessThan);
+      br.run();
+
+      String outputLog = Arrays.toString(br.getLogs().toArray());
+      String expectedLog = String
+        .format("[Log: MESSAGE: a: %s]", expected);
+
+      assertTrue(outputLog.equals(expectedLog));
+    } catch (BrewerParseException | ParseException e) {
+      fail("equalityNotLessThan test failed");
+      e.printStackTrace();
+    }
+  }
+
+  // Greater Than
+  /**
+   * a = (1 > 0);
+   * print a;
+   */
+  @Test
+  public void equalityGreaterThan() {
+    String equalityGreaterThan = "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"name\":\"a\",\"class\":\"bool\"},\"value\":{\"type\":\"comparison\",\"arg1\":{\"type\":\"literal\",\"value\":1,\"class\":\"number\"},\"arg2\":{\"type\":\"literal\",\"value\":0,\"class\":\"number\"},\"name\":\"greater\"}},{\"type\":\"print\",\"name\":{\"type\":\"var\",\"name\":\"a\",\"class\":\"bool\"}}]}";
+    boolean expected = true;
+    try {
+      br = Parser.parseJSONProgram(equalityGreaterThan);
+      br.run();
+
+      String outputLog = Arrays.toString(br.getLogs().toArray());
+      String expectedLog = String
+        .format("[Log: MESSAGE: a: %s]", expected);
+
+      assertTrue(outputLog.equals(expectedLog));
+    } catch (BrewerParseException | ParseException e) {
+      fail("equalityGreaterThan test failed");
+      e.printStackTrace();
+    }
+  }
+
+  // Not Greater Than
+  /**
+   * a = (0 > 0);
+   * print a;
+   */
+  @Test
+  public void equalityNotGreaterThan() {
+    String equalityNotGreaterThan = "{\"main\":[{\"type\":\"set\",\"name\":{\"type\":\"var\",\"name\":\"a\",\"class\":\"bool\"},\"value\":{\"type\":\"comparison\",\"arg1\":{\"type\":\"literal\",\"value\":0,\"class\":\"number\"},\"arg2\":{\"type\":\"literal\",\"value\":0,\"class\":\"number\"},\"name\":\"greater\"}},{\"type\":\"print\",\"name\":{\"type\":\"var\",\"name\":\"a\",\"class\":\"bool\"}}]}";
+    boolean expected = false;
+    try {
+      br = Parser.parseJSONProgram(equalityNotGreaterThan);
+      br.run();
+
+      String outputLog = Arrays.toString(br.getLogs().toArray());
+      String expectedLog = String
+        .format("[Log: MESSAGE: a: %s]", expected);
+
+      assertTrue(outputLog.equals(expectedLog));
+    } catch (BrewerParseException | ParseException e) {
+      fail("equalityNotGreaterThan test failed");
+      e.printStackTrace();
+    }
+  }
+
+  // TODO And, Or, Not, While, Arithmetic basic testing
 
   // The following tests test order of commands.
   /**
@@ -127,11 +406,25 @@ public class ParserTests {
     }
   }
 
+  // Testing precision and if/else
   /**
+   * pi = 3.1415926545
+   * a = 3.1415926544;
    * if (3.1415926545 == a):
    *    a = 0;
    * else
    *    a = 1;
+   * print(a);
+   */
+
+  /**
+   * pi = 3.1415926545;
+   * a = 3.1415926545;
+   * if (3.1415926545 == a):
+   *    a = 0;
+   * else
+   *    a = 1;
+   * print(a);
    */
 
   // More complicated factorial test.
