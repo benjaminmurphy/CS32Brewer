373a554c04c9eb548e839f5019f6e104971c0944
Raphael Kargon <raphael.kargon@gmail.com>
2015-05-02 18:12:55 -0400

    implemented CLI

--- src/main/java/edu/brown/cs/brewer/Main.java
+++ src/main/java/edu/brown/cs/brewer/Main.java
@@ -1,9 +1,15 @@
 package edu.brown.cs.brewer;
 
+import java.util.Scanner;
+
+import org.json.simple.parser.ParseException;
+
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
 import edu.brown.cs.brewer.handlers.BrewerServer;
+import edu.brown.cs.brewer.handlers.Parser;
+import edu.brown.cs.brewer.handlers.Parser.BrewerParseException;
 
 /**
  * Runs a Brewer Server that accepts requests from clients and runs
@@ -19,13 +25,6 @@ public final class Main {
   private static final int DEFAULT_PORT = 4567;
 
   /**
-   * Private constructor; not used.
-   */
-  private Main() {
-    ;
-  }
-
-  /**
    * This runs the server.
    *
    * @param args Command line arguments. Currently just "port", the port number.
@@ -34,6 +33,7 @@ public final class Main {
     OptionParser parser = new OptionParser();
     OptionSpec<Integer> portSpec =
         parser.accepts("port").withRequiredArg().ofType(Integer.class);
+    parser.accepts("gui");
     OptionSet parsedArgs = parser.parse(args);
 
     int port = DEFAULT_PORT;
@@ -41,7 +41,31 @@ public final class Main {
       port = portSpec.value(parsedArgs);
     }
 
-    BrewerServer.runServer(port);
+    if (parsedArgs.has("gui")) {
+      BrewerServer.runServer(port);
+    } else {
+      Scanner in = new Scanner(System.in);
+      BrewerRuntime runtime;
+      String query;
+      System.out.println("Query: ");
+      while ((query = in.nextLine()).trim().length() > 0) {
+        try {
+          runtime = Parser.parseJSONProgram(query);
+          runtime.run();
+          for (Log l : runtime.getLogs()) {
+            if (l.isError()) {
+              System.out.print("ERROR: ");
+            } else {
+              System.out.print("MESSAGE: ");
+            }
+            System.out.println(l.getMsg());
+          }
+        } catch (ParseException | BrewerParseException e) {
+          System.out.println("ERROR: " + e);
+        }
+        System.out.println("Query: ");
+      }
+      in.close();
+    }
   }
-
 }
