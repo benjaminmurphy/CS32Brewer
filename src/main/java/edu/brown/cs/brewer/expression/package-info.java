ae346b1e807bb2a9f1885581d306f7cada29561c
Raphael Kargon <raphael.kargon@gmail.com>
2015-05-02 15:54:21 -0400

    checkstyle fixes

---
+++ src/main/java/edu/brown/cs/brewer/expression/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains objects that represents Brewer Expressions. Each
+ * expression can be run by calling "evaluate()". Expressions are assosciated
+ * with a BrewerRuntime object that stores variables, logs, etc.
+ */
+package edu.brown.cs.brewer.expression;
---
+++ src/main/java/edu/brown/cs/brewer/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The main package for Brewer code, including the Main and Runtime classes.
+ * This represents a server that provides a GUI for clients and runs
+ * user-created programs.
+ */
+package edu.brown.cs.brewer;
